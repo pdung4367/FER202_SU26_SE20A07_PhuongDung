2ec8e9503c4d8962127abe5d8a85e28d13769b81
Dung Phuong <pdung4367@gmail.com>
2026-05-13 16:07:26 +0700

    BT1

---
+++ Slot 2/Exercise/ex1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "phdum"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> phdum </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
